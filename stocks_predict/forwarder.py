7d3b5071bd962c6ef7c7b5181c5209708f69e4d4
gabripo <gabripo93+github@gmail.com>
2024-10-11 15:35:58 +0200

    convert input stock data to pandas dataframe

--- stocks_predict/forwarder.py
+++ stocks_predict/forwarder.py
@@ -1,6 +1,20 @@
-def forward_to_prediction(stockData, predictionMode="linear"):
+import pandas as pd
+
+
+def forward_to_prediction(stockData: dict, predictionMode="linear"):
     """
     function to forward stock data to a prediction function
     the input stock data will be changed in-place
     """
+    stockDataFrame = convert_data_to_pandas_dataframe(stockData)
     pass
+
+
+def convert_data_to_pandas_dataframe(data: dict) -> pd.DataFrame:
+    if "prices" in data:
+        df = pd.DataFrame(data["prices"])
+        df["date"] = pd.to_datetime(df["date"])
+        df.set_index("date", inplace=True)
+    else:
+        df = pd.DataFrame()
+    return df
