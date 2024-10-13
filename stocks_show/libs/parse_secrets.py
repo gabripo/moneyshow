56fcbaa8e73258f6602a1dd1de84c07c69c1c310
gabripo <gabripo93+github@gmail.com>
2024-10-13 15:18:00 +0200

    fix path of secrets

--- stocks_show/libs/parse_secrets.py
+++ stocks_show/libs/parse_secrets.py
@@ -4,7 +4,7 @@ from importlib import import_module
 
 def _get_credential_from_secrets(credential_key):
     try:  # will succeed locally if secret.py file is available
-        secret_module = import_module("stocks_processing.proj_secrets")
+        secret_module = import_module("proj_secrets")
         return getattr(secret_module, credential_key)
     except ModuleNotFoundError:  # will fail on Heroku after deployments
         return None
